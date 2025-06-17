fcb5a15f570e89ade3451549c190705ead3fce63
Maks <csgo20041@gmail.com>
2025-06-17 19:11:21 +0200

    add: changed structure

---
+++ AI/15_puzzle_bot/build/CMakeFiles/15_puzzle_bot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/15_puzzle_bot.dir/link.d"
+  "15_puzzle_bot"
+  "15_puzzle_bot.pdb"
+  "CMakeFiles/15_puzzle_bot.dir/main.cpp.o"
+  "CMakeFiles/15_puzzle_bot.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15_puzzle_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AI/15_puzzle_bot/build/CMakeFiles/15_puzzle_bot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for 15_puzzle_bot.
+# This may be replaced when dependencies are built.
